3922ae2fd72f071205f97a18af9861ee0f06db65
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2022-01-06 00:02:43 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from app.tapas import execute_query
+import gradio as gr
+
+
+def main():
+    description = "Table query demo app, it runs TAPAS model. You can ask a question about tabular data, TAPAS model " \
+                  "will produce the result. Think about it as SQL query running against DB table. The advantage of " \
+                  "TAPAS model - there is no need to upload data to DB or process it in a spreadsheet, data can be " \
+                  "processed in memory by ML model. Pre-trained TAPAS model runs on max 64 rows and 32 columns data. " \
+                  "Make sure CSV file data doesn't exceed these dimensions."
+
+    article = "<p style='text-align: center'><a href='https://katanaml.io' target='_blank'>Katana ML</a> | <a href='https://github.com/katanaml/table-query-model' target='_blank'>Github Repo</a> | <a href='https://huggingface.co/google/tapas-base-finetuned-wtq' target='_blank'>TAPAS Model</a></p><center><img src='https://visitor-badge.glitch.me/badge?page_id=abaranovskij_tablequery' alt='visitor badge'></center>"
+
+    iface = gr.Interface(fn=execute_query,
+                         inputs=[gr.inputs.Textbox(label="Search query"),
+                                 gr.inputs.File(label="CSV file")],
+                         outputs=[gr.outputs.JSON(label="Result"),
+                                  gr.outputs.Dataframe(label="All data")],
+                         examples=[
+                             ["What are the items with total higher than 8?", "taxables.csv"],
+                             ["What is the cost for Maxwell item?", "taxables.csv"],
+                             ["Show items with cost lower than 2 and tax higher than 0.05", "taxables.csv"]
+                         ],
+                         theme="huggingface",
+                         title="Table Question Answering (TAPAS)",
+                         description=description,
+                         article=article,
+                         allow_flagging=False)
+    # Use this config when running on Docker
+    # iface.launch(enable_queue=True, server_name="0.0.0.0", server_port=7000)
+    iface.launch(enable_queue=True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ app/tapas.py
@@ -0,0 +1,56 @@
+from transformers import TapasTokenizer, TFTapasForQuestionAnswering
+import pandas as pd
+import datetime
+
+
+def execute_query(query, csv_file):
+    a = datetime.datetime.now()
+
+    table = pd.read_csv(csv_file.name, delimiter=",")
+    table.fillna(0, inplace=True)
+    table = table.astype(str)
+
+    model_name = "google/tapas-base-finetuned-wtq"
+    model = TFTapasForQuestionAnswering.from_pretrained(model_name)
+    tokenizer = TapasTokenizer.from_pretrained(model_name)
+
+    queries = [query]
+
+    inputs = tokenizer(table=table, queries=queries, padding="max_length", return_tensors="tf")
+    outputs = model(**inputs)
+
+    predicted_answer_coordinates, predicted_aggregation_indices = tokenizer.convert_logits_to_predictions(
+        inputs, outputs.logits, outputs.logits_aggregation
+    )
+
+    # let's print out the results:
+    id2aggregation = {0: "NONE", 1: "SUM", 2: "AVERAGE", 3: "COUNT"}
+    aggregation_predictions_string = [id2aggregation[x] for x in predicted_aggregation_indices]
+
+    answers = []
+    for coordinates in predicted_answer_coordinates:
+        if len(coordinates) == 1:
+            # only a single cell:
+            answers.append(table.iat[coordinates[0]])
+        else:
+            # multiple cells
+            cell_values = []
+            for coordinate in coordinates:
+                cell_values.append(table.iat[coordinate])
+            answers.append(cell_values)
+
+    for query, answer, predicted_agg in zip(queries, answers, aggregation_predictions_string):
+        if predicted_agg != "NONE":
+            answers.append(predicted_agg)
+
+    query_result = {
+        "query": query,
+        "result": answers
+    }
+
+    b = datetime.datetime.now()
+    print(b - a)
+
+    return query_result, table
+
+
